ece5f898a9f36671625f504a1f38ebfbdb757cac
DeanBratian <deanbratian@gmail.com>
2023-02-06 17:25:21 +0200

    Cleared up main

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,6 @@
 int main()
 {
     
-    AlgoViz av;
-    
-    while (av.window->isOpen())
-    {
-        sf::Event event;
-
-        while (av.window->pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                av.window->close();
-        }
-
-        av.window->clear();
-        av.window->draw(av.shaper);
-        av.window->display();
-    }
 
     return 0;
 
